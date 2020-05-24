9781546e8a8b73f040a2f5e98375f50990d30c79
siddtheone <siddharthl@cybage.com>
2020-05-24 14:26:47 +0530

    Updates

---
+++ precache-manifest.7fb3d6383df0f7ffd994f5f374fb3861.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6ea91e088bc573e66b5bafb867bb802e",
+    "url": "/intro-signup/index.html"
+  },
+  {
+    "revision": "d112eb1f9748b6d91772",
+    "url": "/intro-signup/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "b7487997c03fef5c7774",
+    "url": "/intro-signup/static/js/2.dc845e97.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/intro-signup/static/js/2.dc845e97.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d112eb1f9748b6d91772",
+    "url": "/intro-signup/static/js/main.596921bb.chunk.js"
+  },
+  {
+    "revision": "6b44483d7c2badd3aed1",
+    "url": "/intro-signup/static/js/runtime-main.6eb7cd0f.js"
+  },
+  {
+    "revision": "eb8c9c1625fcb08ca7bfc046f015ea87",
+    "url": "/intro-signup/static/media/bg-intro-desktop.eb8c9c16.png"
+  },
+  {
+    "revision": "3e78c71a68ca0575e5ffb30e2e8ccc69",
+    "url": "/intro-signup/static/media/bg-intro-mobile.3e78c71a.png"
+  },
+  {
+    "revision": "eb2c6ce267c763a9a2682bfbb13739cc",
+    "url": "/intro-signup/static/media/icon-error.eb2c6ce2.svg"
+  }
+]);
